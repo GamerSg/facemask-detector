74e52cabcf5a016782a2bb9a8988592168173ffa
Luv Khemani <luvb@hotmail.com>
2021-09-07 20:23:02 +0800

    Add face registration

--- js/face.js
+++ js/face.js
@@ -1,5 +1,7 @@
 const MODEL_URL = './models';
 const minConfidence = 0.5;
+var labeledDescriptors = [];
+var faceMatcher = null;
 
 async function startWebCam() {
     var video = document.querySelector("#videoElement");        //Get video element from HTML
@@ -16,18 +18,44 @@ async function startWebCam() {
     }
 }
 
+async function registerFace(img, label)
+{
+    const results = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+
+    if (!results.descriptor) {
+    return
+    }
+    labeledDescriptors.push(new faceapi.LabeledFaceDescriptors(
+        label,
+        [results.descriptor]
+      ));
+    console.log("Registered "+label);    
+    faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
+}
+
 async function detectFacesVideo(inputSrc, overlay)
 {
     var fdOptions = new faceapi.SsdMobilenetv1Options({ minConfidence });    
-    const results = await faceapi.detectAllFaces(inputSrc, fdOptions);
+    const results = await faceapi.detectAllFaces(inputSrc, fdOptions).withFaceLandmarks().withFaceDescriptors();
     console.log("Detected faces : " + results);
     const dims = faceapi.matchDimensions(overlay, inputSrc, true);
     faceapi.draw.drawDetections(overlay, faceapi.resizeResults(results, dims));
+
+    if(faceMatcher)
+    {
+        results.forEach(fd => {
+            const bestMatch = faceMatcher.findBestMatch(fd.descriptor)
+            console.log(bestMatch.toString())
+          })
+    }
+
     setTimeout(detectFaces, 50, inputSrc, overlay);
 }
 
 async function init() {
-    console.log(faceapi.nets);
     await startWebCam();
     await faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL);
+    await faceapi.loadFaceLandmarkModel(MODEL_URL);
+    await faceapi.loadFaceRecognitionModel(MODEL_URL);
+    console.log("Loaded models");
 }
